863f2a4d6ddb23fc58cb29903264f6ca630a3bc0
Dmitry Semenov <lonelyteapot@gmail.com>
2022-08-05 14:44:14 +0300

    [General] Set up FastAPI with uvicorn

---
+++ opt_public_server/main/server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from .settings import get_settings
+
+
+app = FastAPI(
+    debug=True,
+    title=get_settings().app_title,
+    description=get_settings().app_description,
+    version=get_settings().app_version,
+    contact={
+        "name": get_settings().author_name,
+        "url": get_settings().website,
+    },
+    license_info={
+        "name": get_settings().app_license,
+    },
+)
+
+
+@app.get("/")
+def index():
+    return RedirectResponse("/graphql")
